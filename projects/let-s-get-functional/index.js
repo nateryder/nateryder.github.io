289218b2895d088b96b17301a1acd51e0a138d62
Ubuntu <ubuntu@ip-172-31-87-144.ec2.internal>
2020-06-11 20:37:09 +0000

    lodown

---
+++ projects/let-s-get-functional/index.js
@@ -0,0 +1,73 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+var customers = require('./data/customers.json');
+var _ = require("lodown-nateryder");
+
+/**
+ * 1. Import your lodown module using the require() method,
+ *    using the string 'lodown-<my-username>', or whatever
+ *    name with which you published your npm lodown project.
+ *
+ * 2. Solve all problems as outlined in the README.
+ *
+ * 3. We started the first one for you as an example! Make the rest in that style.
+ *
+ * 4. To test your work, run the following command in your terminal:
+ *start from github.io directory
+ *   npm start --prefix ./projects/let-s-get-functional
+ *
+ *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
+ */
+
+var maleCount = function(array) {
+//invoke my filter function using a array as my first argument
+return _.filter(array, function(customerObj){
+//filtering out male customers
+return customerObj.gender === "male";
+    
+}).length
+
+
+};
+
+var femaleCount;
+return _.reduce(array, function(counter, customerObj){
+    if(customerObj.gender === "female"){
+    counter += 1;
+    }
+    return counter;
+    }, 0)
+
+var oldestCustomer;
+
+var youngestCustomer;
+
+var averageBalance;
+
+var firstLetterCount;
+
+var friendFirstLetterCount;
+
+var friendsCount;
+
+var topThreeTags;
+
+var genderCount;
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+// here, export any references you need for tests //
+module.exports.maleCount = maleCount;
+module.exports.femaleCount = femaleCount;
+module.exports.oldestCustomer = oldestCustomer;
+module.exports.youngestCustomer = youngestCustomer;
+module.exports.averageBalance = averageBalance;
+module.exports.firstLetterCount = firstLetterCount;
+module.exports.friendFirstLetterCount = friendFirstLetterCount;
+module.exports.friendsCount = friendsCount;
+module.exports.topThreeTags = topThreeTags;
+module.exports.genderCount = genderCount;
